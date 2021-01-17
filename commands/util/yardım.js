ac17e89a9139b8432e852b26b8ed810ddbb6db8f
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-17 04:16:57 +0300

    Yardım komutu

---
+++ commands/util/yardım.js
@@ -0,0 +1,36 @@
+module.exports = {
+	name: 'yardım',
+	description: 'Tüm komutlarımı veya belirli bir komutla ilgili bilgileri listeleyin.',
+	aliases: ['komutlar', 'help'],
+	usage: '[komut adı]',
+	cooldown: 5,
+	execute(client, message, args) {
+		const data = [];
+		const { commands, config } = client;
+
+		if (!args.length) {
+			data.push(`Komut listesi:`);
+			data.push(commands.map(command => command.name).join(', '));
+			data.push(`\Bir komut hakkında bilgi almak için \`${config.prefix}yardım [komut adı]\` yazabilirsin);
+
+			return message.chat.sendMessage(data)
+		}
+
+		const name = args[0].toLowerCase();
+		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+		if (!command) {
+			return message.reply(`böyle bir komut yok!`);
+		}
+
+		data.push(`**Komut:** ${command.name}`);
+
+		if (command.aliases) data.push(`Alternatif: ${command.aliases.join(', ')}`);
+		if (command.description) data.push(`Açıklama: ${command.description}`);
+		if (command.usage) data.push(`Usage: ${config.prefix}${command.name} ${command.usage}`);
+
+		data.push(`**Bekleme süresi:** ${command.cooldown || 3} second(s)`);
+
+		message.chat.sendMessage(data);
+	},
+};
