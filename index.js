ee8f8fda4d9ce37bd47edf329e37029f44f7c99b
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-17 17:31:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Insta = require('@androz2091/insta.js');
-const Discord = require('discord.js')
+const Collection = require('@discordjs/collection')
 const fs = require('fs')
 const glob = require('glob');
 const path = require('path');
 const config = require('./config.json');
 const client = new Insta.Client();
-client.commands = new Discord.Collection();
-client.cooldowns = new Discord.Collection()
+client.commands = new Collection();
+client.cooldowns = new Collection()
 client.config = config;
 
 glob.sync('./commands/**/*.js' ).forEach(function(file) {
