a3b018c0d8a2570b5790fcaf0e6f4cd354e65ab2
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-17 03:46:15 +0300

    Fix

---
+++ commands/util/eval.js
@@ -0,0 +1,21 @@
+const util = require('util')
+
+module.exports = {
+  name: "eval",
+  description: "Sadece geliştirici.",
+  execute(client, message, args) {
+
+  if (parseInt(message.author.id) !== parseInt(client.config.ownerid)) return message.chat.sendMessage(`Bunu sadece geliştirici kullanabilir!`)
+        let result = new Promise((resolve) => resolve(eval(args.join(` `))));
+
+        return result.then((output) => {
+            if (typeof output !== `string`) {
+                output = require(`util`).inspect(output, { depth: 0 });
+            }
+            message.chat.sendMessage('📤 Sonuç : \n' + output)
+        }).catch((err) => {
+            err = err.toString();
+            message.chat.sendMessage('⛔ Hata: ' + err);
+        });
+	},
+};
