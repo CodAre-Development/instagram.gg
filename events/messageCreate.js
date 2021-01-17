203a8b1cf9af386dc289aa8539c9a47502f15156
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-17 04:01:02 +0300

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,27 +1,50 @@
 module.exports = (client, message) => {
-  // Ignore all bots
-  if (message.authorID === client.user.id) return;
+        if (message.authorID === client.user.id) return;
 
-  // Ignore messages not starting with the prefix (in config.json)
-  if (!message.content.startsWith(client.config.prefix)) return;
+        if (!message.content.startsWith(client.config.prefix)) return;
     
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
 
-  // Our standard argument/command name definition.
-  const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-  // Grab the command data from the client.commands Enmap
-  const cmd = client.commands.get(command);
+	if (!command) return;
 
-  // If that command doesn't exist, silently exit and do nothing
-  if (!cmd) return;
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
 
-  // Run the command
-  message.markSeen();
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
 
-  try {
-  cmd.execute(client, message, args);
-  } catch(error) {
-  console.error(error);
-  }
+		return message.channel.send(reply);
+	}
+
+	if (!client.cooldowns.has(command.name)) {
+		client.cooldowns.set(command.name, new Enmap());
+	}
+
+	const now = Date.now();
+	const timestamps = client.cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (client.timestamps.has(message.author.id)) {
+		const expirationTime = client.timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	client.timestamps.set(message.author.id, now);
+	setTimeout(() => client.timestamps.delete(message.author.id), cooldownAmount);
+        message.markSeen();
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
 };
