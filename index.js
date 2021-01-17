0ba74dc56d5c0488be54066155a50fd36c228f2c
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-17 06:31:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,66 @@
 const Insta = require('@androz2091/insta.js');
 const fs = require('fs')
+const glob = require('glob');
+const path = require('path');
 const config = require('./config.json');
 const client = new Insta.Client();
 const Collection = require('@discordjs/collection');
 client.commands = new Collection();
+client.cooldowns = new Collection()
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
+glob.sync('./commands/**/*.js' ).forEach(function(file) {
+  const command = require(path.resolve(file));
+  client.commands.set(command.name, command);
+});
 
 client.on('messageCreate', (message) => {
-        if (message.authorID === client.user.id  || !message.content.startsWith(config.prefix)) return;
-    
-    message.args = message.content.slice(config.prefix.length).trim().split(/ +/);
-    const args = message.content.slice(config.prefix.length).trim().split(' ');
-    const icommandName = message.args.shift().toLowerCase();
-    const icommand = client.commands.get(icommandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(icommandName));
+        if (message.author.id === client.user.id) return;
 
-	if (!icommand) return;
-	
+        if (!message.content.startsWith(client.config.prefix)) return;
+    
+        const args = message.content.slice(client.config.prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+	if (!command) return;
+
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.reply(reply);
+	}
+
+	if (!client.cooldowns.has(command.name)) {
+		client.cooldowns.set(command.name, new Enmap());
+	}
+
+	const now = Date.now();
+	const timestamps = client.cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.authorID) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+        message.markSeen();
 	try {
-		icommand.execute(client, message, args);
+		command.execute(client, message, args);
 	} catch (error) {
 		console.error(error);
-    }
+		message.reply('there was an error trying to execute that command!');
+	}
 });
 
 fs.readdir("events", (err, files) => {
