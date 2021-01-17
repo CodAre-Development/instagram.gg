06a4f8fd217814dafacaef0c0f6365ae7ec393e2
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-17 04:31:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,19 @@ const config = require('./config.json');
 const client = new Insta.Client();
 client.config = config;
 client.commands = new Enmap();
+client.timeouts = new Enmap();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
 }
 
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	client.on(event.name, event.bind(null, client));
+}
 
 client.login(config.username, config.password);
