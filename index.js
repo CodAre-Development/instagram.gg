63b1044942d167b8e2051c0d630bcfc43daf4107
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-17 06:15:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fs = require('fs')
 const config = require('./config.json');
 const client = new Insta.Client();
 const Collection = require('@discordjs/collection');
-client.config = config;
 client.commands = new Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -14,31 +13,10 @@ for (const file of commandFiles) {
 }
 
 client.on('messageCreate', (message) => {
-        if (message.authorID === client.user.id  || !message.content.startsWith(config.prefix)) return;
-        const args = message.content.slice(config.prefix.length).trim().split(/ +/);
-	const commandName = args.shift().toLowerCase();
-
-	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-	if (!command) return;
-
-	if (command.args && !args.length) {
-		let reply = `You didn't provide any arguments, ${message.author}!`;
-
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-		}
-
-		return message.reply(reply);
-	}
+        
 
         message.markSeen();
-	try {
-		command.execute(client, message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
+	
 });
 
 fs.readdir("events", (err, files) => {
