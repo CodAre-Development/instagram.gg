2285e88a263b9b3fcc01e495fe189b8d5a6fb51a
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-17 13:55:10 +0300

    Fix

--- commands/youtube/ses.js
+++ commands/youtube/ses.js
@@ -1,14 +1,16 @@
 const ytdl = require('ytdl-core');
 
 module.exports = {
-	name: 'video',
-	description: 'Youtubeden videoyu gönderir. [Maksimum 1 dakika!]',
+	name: 'ses',
+	description: 'Youtubeden bir videoyu ses olarak gönderir. [Maksimum 1 dakika!]',
 	aliases: ['müzik', 'youtube'],
 	usage: '[youtube linki]',
         args: true,
 	cooldown: 5,
 	execute(client, message, args) {
-    message.chat.sendMessage('Video gönderiliyor...').then(() => {
+    const info = ytdl.getBasicInfo(args0);
+    if(info.videoDetails.lengthSeconds > 60) return message.reply("Instagram bir dakikadan uzun ses kayıtlarına izin vermiyor.");
+    message.chat.sendMessage('Ses gönderiliyor...').then(() => {
     const stream = ytdl(args[0], { filter: format => format.container === 'mp4' });
     const array = [];
 	stream
