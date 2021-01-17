004621895f111bcadfa18e561027ba4518ad9397
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-17 05:45:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -27,4 +27,54 @@ fs.readdir("events", (err, files) => {
     });
 });
 
+client.on('messageCreate', (message) => {
+if (message.author.id === client.user.id) return;
+
+        if (!message.content.startsWith(client.config.prefix)) return;
+    
+        const args = message.content.slice(client.config.prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+	if (!command) return;
+
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.reply(reply);
+	}
+
+	if (!client.cooldowns.has(command.name)) {
+		client.cooldowns.set(command.name, new Enmap());
+	}
+
+	const now = Date.now();
+	const timestamps = client.cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.authorID) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+        message.markSeen();
+	try {
+		command.execute(message.client, message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
 client.login(config.username, config.password);
