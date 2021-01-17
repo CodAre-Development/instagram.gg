1f38bd552a879d51715794259a811e6aab928ed3
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-17 03:06:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# [Insta.gg](https://www.instagram.com/insta.gg.bot/)
+
+💬 An Instagram Bot [Androz](https://www.npmjs.com/package/@androz2091/insta.js)'s Instagram library and discord.js 
+
+insta.js x discord.js
+
+## How to install?
+
+```js
+git clone https://github.com/TapsHTS/insta.gg
+cd insta.gg
+npm i
+```
+config [config.json](https://github.com/TapsHTS/insta.gg/blob/master/README.md#configuration)
+```js
+node index.js
+
+```
+
+## Configuration
+
+```js
+{
+    "prefix": "i!",
+    "username": "Instagram-Username",
+    "password": "Instagram-Password",
+    "token": "Discord-BotToken",
+    "ownerid": "Instagram-Id(send i!info to insta.gg.bot)"
+}
+
+```
+
+## Credits
+
+🧡 Big thanks to **[Androz](https://github.com/Androz2091)** for the librarie.
+# insta.gg
+
---
+++ commands/insta/musique.js
@@ -0,0 +1,27 @@
+const ytdl = require('ytdl-core');
+
+module.exports = {
+	name: 'musique',
+	description: 'envoie la musique',
+    execute(message, clienti, args) {
+  if(args[0] == "help"){
+    message.chat.sendMessage("Veuillez spécifier une musique de moins de 1 minute !");
+    return;
+  }
+	    message.chat.sendMessage('Création de la musique en cours...').then(() => {
+    const stream = ytdl(args[1], { filter: format => format.container === 'mp4' });
+    const array = [];
+	stream
+      .on('data', chunk => {
+        array.push(chunk);
+      })
+      .on('end', () => {
+        message.chat.sendVoice(Buffer.concat(array));
+      })
+        }).catch((err) => {
+            err = err.toString();
+            message.chat.sendMessage('⛔' + err);
+        });
+
+	},
+};
