e1ce95eabeb01cfc0da695002c6e9f2cdc88ba14
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-18 13:19:17 +0300

    Update EventHandler.js

--- src/handler/EventHandler.js
+++ src/handler/EventHandler.js
@@ -1,15 +1,16 @@
 const fs = require("fs");
+const path = require("path");
 module.exports = function Init(client) {
-  fs.readdir("../events", (err, files) => {
+  var pth = path.join(__dirname, '/../events/');
+  fs.readdir(pth, (err, files) => {
     if (err) return console.error;
     files.forEach(file => {
         if (!file.endsWith(".js")) return;
-        let event = require(`../events/${file}`);
+        let event = require(`${pth}/${file}`);
         let eventName = file.split(".")[0];
 
         client.on(eventName, event.bind(null, client));
         delete require.cache[require.resolve(`./events/${file}`)];
     });
 });
-console.log(client);
 };
