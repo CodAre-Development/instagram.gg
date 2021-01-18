95f477fe42c843114331b9e727539eb7b1f247b6
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-18 14:09:46 +0300

    Update EventHandler.js

--- src/handler/EventHandler.js
+++ src/handler/EventHandler.js
@@ -1,6 +1,6 @@
 const glob = require("glob");
 module.exports = function Init(client) {
-  let eventFiles = glob.sync("./src/events/*.js");
+  let eventFiles = glob.sync("./src/events/**/*.js");
 
   eventFiles.forEach((file) => {
     let event = require(`../../${file}`);
