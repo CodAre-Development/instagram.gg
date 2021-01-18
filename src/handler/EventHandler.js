66f042664619b3890419b0e5973ae12e6506367b
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-18 13:50:56 +0300

    Update EventHandler.js

--- src/handler/EventHandler.js
+++ src/handler/EventHandler.js
@@ -1,17 +1,14 @@
 const glob = require("glob");
 module.exports = function Init(client) {
-  const eventFiles = glob.sync("./src/events/*.js");
+  let eventFiles = glob.sync("./src/events/*.js");
 
   eventFiles.forEach((file) => {
-    const event = require(`../../${file}`);
+    let event = require(`../../${file}`);
     let eventName = file.replace(/^.*[\\\/]/, '')
     eventName = eventName.split(".")[0];
     
     client.on(eventName, event.bind(null, client));
 
     delete require.cache[require.resolve(`../../${file}`)];
-
-    // debug
-    // Logger.log("events", `Loaded ${bot.toCapitalize(type)}: ${event.name}`);
   });
 };
