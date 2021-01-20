98726971f777f90bd3c6334991d54d703a6686d9
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-20 19:51:49 +0300

    Multi media support

--- src/events/message/messageCreate.js
+++ src/events/message/messageCreate.js
@@ -1,35 +1,37 @@
 const util = require("../../util/functions.js");
-module.exports = async (client, message) => {
-        if (message.author.id === client.user.id) return;
+module.exports = async(client, message) => {
+	if (message.author.id === client.user.id) return;
 
-        const isMediaShare = message.data.item_type === 'media_share';
+	const isMediaShare = message.data.item_type === 'media_share';
 
-        if (isMediaShare) {
-    const mediaData = {
-      messageSender: message.author.username,
-      creatorIgHandle: util.extractCreator(message.data),
-      images: util.extractImages(message.data),
-      mediaShareUrl: util.extractMediaShareUrl(message.data),
-      timestamp: util.extractPostTimestamp(message.data),
-      location: util.extractLocation(message.data),
-    }
-        await message.chat.sendMessage("✅ Resim gönderiliyor...");
-        await message.chat.sendPhoto(mediaData.images[0]);
-        return;
-        };
-        if (!message.content.startsWith(client.config.prefix)) return;
-    
-        const args = message.content.slice(client.config.prefix.length).trim().split(/ +/);
-        const cooldowns = client.cooldowns;
+	if (isMediaShare) {
+		const mediaData = {
+			messageSender: message.author.username,
+			creatorIgHandle: util.extractCreator(message.data),
+			images: util.extractImages(message.data),
+			mediaShareUrl: util.extractMediaShareUrl(message.data),
+			timestamp: util.extractPostTimestamp(message.data),
+			location: util.extractLocation(message.data),
+		}
+                const start = Date.Now();
+		await message.chat.sendMessage("✅ Resim(ler) gönderiliyor...");
+		mediaData.images.forEach(image => await message.chat.sendPhoto(image));
+                await message.chat.sendMessage(`✅ Resim(ler) başarıyla gönderildi! (${Date.Now() - start} ms)`);
+		return;
+	};
+	if (!message.content.startsWith(client.config.prefix)) return;
+
+	const args = message.content.slice(client.config.prefix.length).trim().split(/ +/);
+	const cooldowns = client.cooldowns;
 	const commandName = args.shift().toLowerCase();
 
 	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
 	if (!command) return;
 
-        const now = Date.now();
-        const timestamps = cooldowns.get(command.name);
-        const cooldownAmount = command.cooldown * 1000;
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = command.cooldown * 1000;
 
 	if (command.args && !args.length) {
 		let reply = `Bir argüman belirtmediniz!`;
@@ -42,20 +44,20 @@ module.exports = async (client, message) => {
 	}
 
 	if (timestamps.has(message.author.id)) {
-          const expTime = timestamps.get(message.author.id) + cooldownAmount;
+		const expTime = timestamps.get(message.author.id) + cooldownAmount;
 
-          if (now < expTime) {
-            const timeleft = (expTime - now) / 1000;
-            return message.reply(
-              `lütfen ${command.name} komutunu kullanabilmek için ${timeleft.toFixed(1)} saniye bekleyin.`
-            );
-          }
-        }
+		if (now < expTime) {
+			const timeleft = (expTime - now) / 1000;
+			return message.reply(
+				`lütfen ${command.name} komutunu kullanabilmek için ${timeleft.toFixed(1)} saniye bekleyin.`
+			);
+		}
+	}
 
-        timestamps.set(message.author.id, now);
-        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
-        await message.markSeen();
+	await message.markSeen();
 	try {
 		await command.execute(client, message, args);
 	} catch (error) {
